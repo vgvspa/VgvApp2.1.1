25377da573a757c25e4fb0aceda1728cf21a6f9e
vgvspa <MAURO.OLEA@VGV.CL>
2026-06-10 11:47:54 -0400

    Update service-worker-v4.js

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,6 +1,23 @@
+//============================================================
+//killer SW 
+//============================================================
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 // ============================================================
 // CONFIGURACIÓN
-// ============================================================
+//============================================================
 const CACHE_NAME = "vgv-cache-v4";
 
 const urlsToCache = [
